6f0f27a0811a61c489d4044bc2bbfe20fb8dd9fc
Tim Hansson Meng <tim.hanssonmeng@appliedtechnology.se>
2023-07-05 17:45:21 +0200

    fix: env file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  const testName = 'TEST';
+  const testName = 'test3';
 
   const printMeme = async () => {
     fetch('http://localhost:8080/api/memes' + '/' + testName)
